f91e3114c6888a1fd017af717fa99285d45e3b67
koenraad.pecceu <koenraad.pecceu@gmail.com>
2025-03-18 09:38:10 +0100

    Add enums

---
+++ ShirtShop.Entities/Shirt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ShirtShop.Entities
+{
+    public class Shirt
+    {
+        public Guid ShirtId { get; set; }
+        public string Name { get; set; }
+        public Fabric Fabric { get; set; }
+        public Size Size { get; set; }
+        public Color Color { get; set; }
+        public SleeveLength SleeveLength { get; set; }
+        public Brand Brand { get; set; }
+        public bool EasyIroning { get; set; }
+        public decimal Price { get; set; } 
+    }
+}
---
+++ ShirtShop.Entities/ShirtEnums.cs
@@ -0,0 +1,70 @@
+﻿namespace ShirtShop.Entities
+{
+    // Fabric, Size, Color, SleeveLength, Brand
+
+    public enum Fabric
+    {
+        Cotton,
+        Polyester,
+        Wool,
+        Silk,
+        Linen,
+        Rayon,
+        Nylon,
+        Acrylic,
+        Spandex
+    }
+
+    public enum Size
+    {
+        XS,
+        S,
+        M,
+        L,
+        XL,
+        XXL,
+        XXXL
+    }
+
+    public enum Color
+    {
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Indigo,
+        Violet,
+        Black,
+        White,
+        Gray,
+        Brown,
+        Pink,
+        Purple,
+        Beige,
+        Maroon,
+        Gold,
+        Silver,
+        Bronze
+    }
+
+    public enum SleeveLength
+    {
+        Sleeveless,
+        ShortSleeve,
+        HalfSleeve,
+        ThreeQuarterSleeve,
+        LongSleeve
+    }
+
+    public enum Brand
+    {
+        HugoBoss,
+        Eton,
+        Turnbull,
+        AbercrombieAndFitch,
+        TommyHilfiger,
+        RalphLauren,
+        CalvinKlein
+    }
+}
